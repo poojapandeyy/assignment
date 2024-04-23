4c09bb7468000fb07b351eee4eb712d9f9f41fd9
unknown <paandeypoojaa@gmail.com>
2024-04-23 09:04:49 +0545

    problemsolved

--- javascript/basic/operator.js
+++ javascript/basic/operator.js
@@ -9,3 +9,20 @@ console.log(sum)
 const a = 10
 const b = '10'
 console.log(a === b)
+
+// generate a random number between 0 and 5, if it is lesser than 3,
+//console.log "lesser than 3" else "Greater"
+// hint  Math.random
+
+const randomNum = Math.random()*6
+const roundOffVal = Math.floor(randomNum)
+if (roundOffVal<3){
+    console.log(roundOffVal + "is less than 3")
+}else if(roundOffVal === 3){
+    console.log(roundOffVal + "is equal to 3")
+}
+else{
+    console.log(roundOffVal + "is greater than 3")
+}
+
+
