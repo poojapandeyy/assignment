f086372b2f8d4f2fa750a6057c1e19db1b9c23fd
unknown <paandeypoojaa@gmail.com>
2024-04-24 07:20:17 +0545

    level2solves

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -2,11 +2,11 @@ const arr = [3,5,2]
 //Q1 calculate length of array and console log
 //expected output is 3
 
-console.log(arr.length)
+console.log(arr.length) //finding out the length of array
 
 //Q2 reverse the array and console log
 //expected output [2,5,3]
-const rev = [...arr].reverse()
+const rev = [...arr].reverse()  // ... to copy the original array and then make reverse of copied array
 console.log(rev)
 
 ///Q3 join the array and console log
---
+++ javascript/array/level2.js
@@ -0,0 +1,12 @@
+const arr =  [3,5,2]
+//console.log the maximum number of an array
+//expectedd output 5
+
+let i=0
+let max=0
+for(i=0; i<arr.length; i++){
+  if(arr[i]>max){
+    max = arr[i]
+  }
+}
+console.log(max)
