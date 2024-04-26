f1293cc5124b140b5192d0b69d123383174442c8
unknown <paandeypoojaa@gmail.com>
2024-04-26 22:00:41 +0545

    problemsolved

--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -10,5 +10,29 @@ for(i=0; i<arr.length; i++){
   }
 }
 
-
 console.log(max)
+
+//Q2 Remove a specific element 2 from the array. Log the modified array to the console.
+// expected output: [3,5]
+
+const elementToRemove = 2;
+// let j=0
+// for(j=0; j<arr.length; j++){
+//   if(arr[j]!==elementToRemove){
+//     console.log( arr[j])
+//   }
+// }
+arr.forEach((item)=>{
+  if(item!== elementToRemove)
+  console.log(item)
+})
+
+
+//Q3 Multiply each element of the array by a constant value 5 and log the modified array to the console.
+//expected output [15,10]
+
+
+//find intersection (common elements) of arr and arr2
+//expected output: [5]
+const arr2 = [5,21,32]
+
